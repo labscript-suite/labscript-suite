ab1571ac323d1de2eeca4202b03ffead2a976f12
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2020-06-27 18:49:29 +1000

    Force release-branch-semver version scheme of setuptools_scm

--- labscript_suite/__version__.py
+++ labscript_suite/__version__.py
@@ -1,19 +1,16 @@
-import os
 from pathlib import Path
+
 try:
     import importlib.metadata as importlib_metadata
 except ImportError:
     import importlib_metadata
 
-VERSION_SCHEME = {
-    "version_scheme": os.getenv("SCM_VERSION_SCHEME", "guess-next-dev"),
-    "local_scheme": os.getenv("SCM_LOCAL_SCHEME", "node-and-date"),
-}
 
 root = Path(__file__).parent.parent
 if (root / '.git').is_dir():
     from setuptools_scm import get_version
-    __version__ = get_version(root, **VERSION_SCHEME)
+
+    __version__ = get_version(root, version_scheme="release-branch-semver")
 else:
     try:
         __version__ = importlib_metadata.version(__package__)
